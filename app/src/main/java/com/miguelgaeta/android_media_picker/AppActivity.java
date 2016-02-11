3915779dca6f39d8e59cea49582df69569b13e30
mrkcsc <miguel@miguelgaeta.com>
2016-02-10 17:15:06 -0800

    Initial commit.

---
+++ app/src/main/java/com/miguelgaeta/android_media_picker/AppActivity.java
@@ -0,0 +1,14 @@
+package com.miguelgaeta.android_media_picker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.app_activity);
+    }
+}
---
+++ media-picker/src/main/java/com/miguelgaeta/media_picker/MediaPicker.java
@@ -0,0 +1,7 @@
+package com.miguelgaeta.media_picker;
+
+/**
+ * Created by Miguel Gaeta on 2/10/16.
+ */
+public class MediaPicker {
+}
