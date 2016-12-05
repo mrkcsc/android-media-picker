1f05c7b704f06a7789fca5d8c55bc6116bfe34c8
mrkcsc <miguel@miguelgaeta.com>
2016-12-04 20:31:17 -0800

    Add support for extracting mime type from result.

---
+++ media-picker/src/main/java/com/miguelgaeta/media_picker/MimeType.java
@@ -0,0 +1,96 @@
+package com.miguelgaeta.media_picker;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+
+/**
+ * Exposes methods for extracting mime type given a
+ * target url or uri.  Uses a combination of
+ * extraction methods to heuristically determine a
+ * best guess for the associated type.
+ */
+@SuppressWarnings("unused")
+public class MimeType {
+
+    private static final MimeTypeMap MIME_TYPE_MAP = MimeTypeMap.getSingleton();
+
+    /**
+     * Get a best guess of the mime type associated with the target {@link Uri}.
+     *
+     * @param context Optional {@link Context} that checks for mime type from {@link ContentResolver}.
+     * @param uri Target {@link Uri} to obtain mime type for.
+     *
+     * @return Discovered mime type or null.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static String getMimeType(final Context context, final Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+
+        if (context != null) {
+            final String mimeType = getMimeTypeFromContentResolver(context.getContentResolver(), uri);
+
+            if (mimeType != null) {
+                return mimeType;
+            }
+        }
+
+        return getMimeTypeFromExtension(uri.toString());
+    }
+
+    /**
+     * @see #getMimeType(Context, Uri)
+     */
+    public static String getMimeType(final Context context, final String url) {
+        if (url == null) {
+            return null;
+        }
+
+        final Uri uri = Uri.parse(url);
+
+        return getMimeType(context, uri);
+    }
+
+    /**
+     * Uses {@link ContentResolver#getType(Uri)} to extract a mime type.
+     *
+     * @param contentResolver {@link ContentResolver}.
+     * @param uri {@link Uri}.
+     *
+     * @return Discovered mime type or null.
+     */
+    private static String getMimeTypeFromContentResolver(final ContentResolver contentResolver,
+                                                         final Uri uri) {
+        if (uri == null || uri.getScheme() == null || contentResolver == null) {
+            return null;
+        }
+
+        if (!uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            return null;
+        }
+
+        return contentResolver.getType(uri);
+    }
+
+    /**
+     * Uses {@link MimeTypeMap#getMimeTypeFromExtension(String)} to extract a mime type.
+     *
+     * @param url {@link Uri}.
+     *
+     * @return Discovered mime type or null.
+     */
+    private static String getMimeTypeFromExtension(final String url) {
+        String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+
+        if (extension != null) {
+            extension = extension.toLowerCase();
+
+            return MIME_TYPE_MAP.getMimeTypeFromExtension(extension);
+        }
+
+        return null;
+    }
+}
